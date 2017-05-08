5fde89aba8edcd19c35834f4f97eb539d5d3fd55
FanYucai <fanyucai@126.com>
2017-05-08 15:05:28 +0800

    most simple version proxy2.py

--- proxy2.py
+++ proxy2.py
@@ -12,23 +12,13 @@ def asClient(browserConnection):
             data = browserConnection.recv(65535)   
             if not (data and len(data)):
                 break
-            # print "############ req: ############\n%s\n#######################################" % (data)
-            # cturl = re.search(r'GET\s+([^\s]+)\s+HTTP[^\r]*\r\n',data)
-            # if cturl:
-            #     cturl = cturl.group(1)
+
             remoteaddr = re.search(r'Host:\s*([^\n\r]*)\s*[\r\n]+',data)
             if remoteaddr:
                 remoteaddr=remoteaddr.group(1)
                 print "remoteaddr = %s\n" % (remoteaddr)
-
             else:
                 break   
-            # if cturl == None: #at the same time resend packet to the server
-            #     if cturl in cache:
-            #         browserConnection.sendall(cache[cturl][0])
-            #         cfg=True         
-            #         continue          
-
             internetSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             nsaddr = (remoteaddr,80)
             internetSock.connect(nsaddr)
@@ -38,52 +28,8 @@ def asClient(browserConnection):
             try:
                 while True:
                     ssdata = internetSock.recv(65535)
-                    sslen = re.search(r'Content-Length:\s*([0-9]+)\s*\r\n',ssdata)
-
-                    if sslen:
-                        sslen = int(sslen.group(1))
-                        rawdata = ssdata
-                        sslen -= len(ssdata[ssdata.find('\r\n\r\n')+4:]) # truncate http head
-                        browserConnection.sendall(ssdata)
-                        
-                    else:                        
-                        if ssdata and len(ssdata):
-                            browserConnection.sendall(ssdata)
-                            continue
-                        else:
-                            break
-                    
-                    while sslen>0:
-                        ssdata = internetSock.recv(65535)
-                        rawdata+=ssdata
-                        sslen-=len(ssdata)
-  
-                        if ssdata and len(ssdata):
-                            browserConnection.sendall(ssdata)   
-                # while True:
-                #     ssdata = internetSock.recv(65535)
-                #     sslen = re.search(r'Content-Length:\s*([0-9]+)\s*\r\n',ssdata)
-
-                #     if sslen:
-                #         sslen = int(sslen.group(1))
-                #         rawdata = ssdata
-                #         sslen -= len(ssdata[ssdata.find('\r\n\r\n')+4:])
-                #         browserConnection.sendall(ssdata)
-                #         # print "sslen:%d\n" % (sslen)
-                #     else:                        
-                #         if ssdata and len(ssdata):
-                #             browserConnection.sendall(ssdata)
-                #             continue
-                #         else:
-                #             break
-                    
-                #     while sslen>0:
-                #         ssdata = internetSock.recv(65535)
-                #         rawdata+=ssdata
-                #         sslen-=len(ssdata)
-  
-                #         if ssdata and len(ssdata):
-                #             browserConnection.sendall(ssdata)                    
+                    if ssdata and len(ssdata):
+                        browserConnection.sendall(ssdata)                      
             except:
                 pass
             finally:                
@@ -94,16 +40,16 @@ def asClient(browserConnection):
         browserConnection.close()  
         return 
 
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_address = ('localhost', 8889)
+    sock.bind(server_address)
+    sock.listen(100)
+    print  '套接字已建立，开始监听 %d 端口...\n' % (8889)
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_address = ('localhost', 8889)
-sock.bind(server_address)
-sock.listen(100)
-print  '套接字已建立，开始监听 %d 端口...\n' % (8889)
-
-while True:
-    browserConnection , client_address =  sock.accept()
-    browserConnection.settimeout(1.5)
-    # print  '建立连接，客户端地址为: ',client_address
-    t = threading.Thread(target=asClient,args=(browserConnection,)) 
-    t.start()
+    while True:
+        browserConnection , client_address =  sock.accept()
+        browserConnection.settimeout(1.5)
+        # print  '建立连接，客户端地址为: ',client_address
+        t = threading.Thread(target=asClient,args=(browserConnection,)) 
+        t.start()
